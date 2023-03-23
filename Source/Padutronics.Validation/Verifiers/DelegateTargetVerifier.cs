c4de52774767f10f079721dcfcb063b56161ba9b
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 13:19:20 +0100

    Add delegate verifiers

---
+++ Source/Padutronics.Validation/Verifiers/DelegateTargetVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Padutronics.Validation.Verifiers;
+
+public sealed class DelegateTargetVerifier<TTarget, TValue> : ITargetVerifier<TTarget, TValue>
+{
+    private readonly Func<TTarget, TValue, VerificationResult> verifyMethod;
+
+    public DelegateTargetVerifier(Func<TTarget, TValue, VerificationResult> verifyMethod)
+    {
+        this.verifyMethod = verifyMethod;
+    }
+
+    public VerificationResult Verify(TTarget target, TValue value)
+    {
+        return verifyMethod(target, value);
+    }
+}
---
+++ Source/Padutronics.Validation/Verifiers/DelegateVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Padutronics.Validation.Verifiers;
+
+internal sealed class DelegateVerifier<T> : IVerifier<T>
+{
+    private readonly Func<T, VerificationResult> verifyMethod;
+
+    public DelegateVerifier(Func<T, VerificationResult> verifyMethod)
+    {
+        this.verifyMethod = verifyMethod;
+    }
+
+    public VerificationResult Verify(T value)
+    {
+        return verifyMethod(value);
+    }
+}
