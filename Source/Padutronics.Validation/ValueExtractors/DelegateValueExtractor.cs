dc88465056f4eaeee286c347aaf73b721361cbaa
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 13:34:29 +0100

    Add delegate value extractor

---
+++ Source/Padutronics.Validation/ValueExtractors/DelegateValueExtractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Padutronics.Validation.ValueExtractors;
+
+public sealed class DelegateValueExtractor<TTarget, TValue> : IValueExtractor<TTarget, TValue>
+{
+    private readonly Func<TTarget, TValue> extractionMethod;
+
+    public DelegateValueExtractor(Func<TTarget, TValue> extractionMethod)
+    {
+        this.extractionMethod = extractionMethod;
+    }
+
+    public TValue Extract(TTarget target)
+    {
+        return extractionMethod(target);
+    }
+}
